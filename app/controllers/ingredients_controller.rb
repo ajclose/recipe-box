64e70e0512f5bc02d25285b15744260c67ecc092
Alex Close <ajclose@gmail.com>
2017-07-31 20:06:05 -0500

    Style recipe show page

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,13 @@
 class IngredientsController < ApplicationController
+
+  def create
+    @recipe = Recipe.find_by id: params[:recipe_id]
+    @ingredient = Ingredient.new
+    @ingredient.name = params[:ingredient][:name]
+    @ingredient.quantity = params[:ingredient][:quantity]
+    @ingredient.unit = params[:ingredient][:unit]
+    @ingredient.recipe_id = params[:recipe_id]
+    @ingredient.save
+    redirect_to recipe_path(@recipe)
+  end
 end
